6c68be77257d2f97950dc1a1e093cf2cf60a4faa
Julia Aasmaa <my_e_10@hotmail.com>
2019-12-20 08:25:58 +0200

    commit

---
+++ ChuckAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace ChuckAPI
+{
+    class Program
+    {
+        private static string url => "https://api.chucknorris.io/jokes/random";
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                Joke joke = JsonConvert.DeserializeObject<Joke>(response);
+                Console.WriteLine($"Joke: {joke.Value}");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
